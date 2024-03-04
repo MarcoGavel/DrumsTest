9252deed629db9fcf87a59e3dd2649890e7b423c
Gavel <156299795+MarcoGavel@users.noreply.github.com>
2024-03-04 18:01:47 +0100

    Update index.js
    JS page of drum test, now u can click relative buttons for make sounds!;

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+//JS page of drum test, now u can click relative buttons for make sounds!;
 var numberOfDrumButtons =document.querySelectorAll(".drum").length;
 
 
@@ -9,51 +9,69 @@ for(var i=0; i<numberOfDrumButtons; i++){
 
     var buttonInnerHTML = this.innerHTML;
 
-    switch (buttonInnerHTML) {
-      case "w":
-      var tom1 = new Audio('sounds/tom-1.mp3');
-      tom1.play();
-        break;
-
-        case "a":
-        var tom2 = new Audio('sounds/tom-2.mp3');
-        tom2.play();
-          break;
-
-          case "s":
-          var tom3 = new Audio('sounds/tom-3.mp3');
-          tom3.play();
-            break;
-
-            case "d":
-            var tom4 = new Audio('sounds/tom-4.mp3');
-            tom4.play();
-              break;
-
-              case "j":
-              var snar = new Audio('sounds/snare.mp3');
-              snar.play();
-                break;
-
-                case "k":
-                var cras = new Audio('sounds/crash.mp3');
-                cras.play();
-                  break;
-
-                  case "l":
-                  var kic = new Audio('sounds/kick-bass.mp3');
-                  kic.play();
-                    break;
-
-
-
-      default: alert("Errorrrr");
-
-    }
+    makeSound(buttonInnerHTML)
 
 
 });
 }
+
+document.addEventListener("keydown", function(event){
+
+  makeSound(event.key);
+
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+    var tom1 = new Audio('sounds/tom-1.mp3');
+    tom1.play();
+      break;
+
+      case "a":
+      var tom2 = new Audio('sounds/tom-2.mp3');
+      tom2.play();
+        break;
+
+        case "s":
+        var tom3 = new Audio('sounds/tom-3.mp3');
+        tom3.play();
+          break;
+
+          case "d":
+          var tom4 = new Audio('sounds/tom-4.mp3');
+          tom4.play();
+            break;
+
+            case "j":
+            var snar = new Audio('sounds/snare.mp3');
+            snar.play();
+              break;
+
+              case "k":
+              var cras = new Audio('sounds/crash.mp3');
+              cras.play();
+                break;
+
+                case "l":
+                var kic = new Audio('sounds/kick-bass.mp3');
+                kic.play();
+                  break;
+
+
+
+    default: alert("Errorrrr");
+
+  }
+}
+
+
+
+
+
+
+
+
 
 
 //var audio = new Audio('sounds/tom-1.mp3');
